475b10a3ab9b99d031142ac4333f2bd31f6a7603
isaiasgj0002 <isaiasguzman863@gmail.com>
2023-04-12 19:48:58 -0500

    Corrigiendo error JiTsiMeet

--- app/src/main/java/com/twoploapps/a2plomessenger/LlamadaActivity.java
+++ app/src/main/java/com/twoploapps/a2plomessenger/LlamadaActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
 import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.URL;
 
 public class LlamadaActivity extends AppCompatActivity {
 
@@ -14,11 +17,21 @@ public class LlamadaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_llamada);
         String idllamada = getIntent().getStringExtra("codigo");
-        String meetingUrl = "https://meet.jit.si/" + idllamada;
-        JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-                .setRoom(meetingUrl)
-                .setFeatureFlag("welcomepage.enabled", false)
+        try{
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(new URL("https://meet.jit.si"))
+                    .setFeatureFlag("welcomepage.enabled", false)
+                    .build();
+            JitsiMeet.setDefaultConferenceOptions(options);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        JitsiMeetConferenceOptions options
+                = new JitsiMeetConferenceOptions.Builder()
+                .setRoom(idllamada)
+                .setAudioMuted(true)
+                .setVideoMuted(true)
                 .build();
-        JitsiMeetActivity.launch(this,options);
+        JitsiMeetActivity.launch(this, options);
     }
 }
