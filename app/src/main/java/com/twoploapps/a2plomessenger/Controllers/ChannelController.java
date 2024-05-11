6f72af8550f96685263727489a033922663f6dc6
isaiasgj0002 <isaiasguzman863@gmail.com>
2024-05-11 13:38:49 -0500

    Nueva 2Plo Messenger - Etapa 4 - Abrir pantalla de canal

--- app/src/main/java/com/twoploapps/a2plomessenger/Controllers/ChannelController.java
+++ app/src/main/java/com/twoploapps/a2plomessenger/Controllers/ChannelController.java
@@ -9,17 +9,26 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.twoploapps.a2plomessenger.InicioActivity;
 import com.twoploapps.a2plomessenger.Models.Canal;
+import com.twoploapps.a2plomessenger.NewActivitys.MensajesCanalActivity;
 import com.twoploapps.a2plomessenger.R;
 
 public class ChannelController {
 
+    public interface OnChannelDataReceived {
+        void onDataReceived(Canal canal);
+    }
+
+    static FirebaseAuth auth = FirebaseAuth.getInstance();
+    static DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
     public static void Create(Canal canal, String channelId, Context context){
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
         ref.child("Canales").child(channelId).setValue(canal).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
@@ -36,4 +45,27 @@ public class ChannelController {
             }
         });
     }
+
+    public static void getData(String id, OnChannelDataReceived callback){
+        ref.child("Canales").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Canal canal = new Canal();
+                if(snapshot.exists()){
+                    canal.setId(id);
+                    canal.setNombre(snapshot.child("nombre").getValue(String.class));
+                    canal.setImagen(snapshot.child("imagen").getValue(String.class));
+                    canal.setDescripcion(snapshot.child("descripcion").getValue(String.class));
+                    canal.setCreador_Id(snapshot.child("creador_id").getValue(String.class));
+                }
+                callback.onDataReceived(canal);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                callback.onDataReceived(null);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/twoploapps/a2plomessenger/NewActivitys/MensajesCanalActivity.java
+++ app/src/main/java/com/twoploapps/a2plomessenger/NewActivitys/MensajesCanalActivity.java
@@ -1,19 +1,59 @@
 package com.twoploapps.a2plomessenger.NewActivitys;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.database.DatabaseReference;
+import com.squareup.picasso.Picasso;
+import com.twoploapps.a2plomessenger.Controllers.ChannelController;
+import com.twoploapps.a2plomessenger.Models.Canal;
 import com.twoploapps.a2plomessenger.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MensajesCanalActivity extends AppCompatActivity {
+    private TextView nombrecanal;
+    private CircleImageView canal_imagen;
+    private DatabaseReference RootRef,NotificacionesRef;
+    private Canal canal;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensajes_canal);
         String id = getIntent().getStringExtra("channel_id");
-        Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
+        Toolbar toolbar = findViewById(R.id.channel_chat_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.layout_chat_bar, null);
+        actionBar.setCustomView(view);
+
+        nombrecanal = findViewById(R.id.usuario_nombre);
+        canal_imagen = findViewById(R.id.usuario_imagen);
+        TextView ultimaconexion = findViewById(R.id.usuario_conexion);
+
+        ChannelController.getData(id, new ChannelController.OnChannelDataReceived() {
+            @Override
+            public void onDataReceived(Canal canal) {
+                if (canal != null) {
+                    nombrecanal.setText(canal.getNombre());
+                    ultimaconexion.setText(getString(R.string.canalpublico));
+                    Picasso.get().load(canal.getImagen()).into(canal_imagen);
+                }
+            }
+        });
     }
 }
