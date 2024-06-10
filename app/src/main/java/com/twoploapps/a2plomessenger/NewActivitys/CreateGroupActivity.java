d051be38616dc524fd768e8eeb55aae89c522f35
isaiasgj0002 <isaiasguzman863@gmail.com>
2024-06-10 18:31:33 -0500

    Nueva 2Plo Messenger - Etapa 4 - Crear fragment grupo

---
+++ app/src/main/java/com/twoploapps/a2plomessenger/NewActivitys/CreateGroupActivity.java
@@ -0,0 +1,16 @@
+package com.twoploapps.a2plomessenger.NewActivitys;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.twoploapps.a2plomessenger.R;
+
+public class CreateGroupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
